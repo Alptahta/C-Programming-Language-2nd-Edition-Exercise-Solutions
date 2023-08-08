70bdd201210dc41c0ed48705ed3cb05aa8d44e8f
Alptahta <alp.tht48@gmail.com>
2023-08-08 10:54:09 +0300

    Add solutions for Chapter 1 exercises 1-4

---
+++ Chapter-1/Exercise-1.3/heading-for-table.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*Modify the temperature conversion program to print a heading above the table.*/
+main(){
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Fahr\tCelsius\n");
+    fahr = lower;
+    while(fahr <= upper)
+    {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
